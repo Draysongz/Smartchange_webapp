73e9666e1131a2087516a1394c7c01b65fca0e98
Ackermann <dharmiegra4@gmail.com>
2023-02-15 16:08:40 +0100

    authentication

---
+++ server/db/connection.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+
+const connectDB = async () =>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log(`connected DB }`)
+    }catch(err){
+        console.log(`Connection not successful`)
+        throw err
+    }
+}
+
+
+export default connectDB
---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './db/connection.js';
+import auth from './Routes/Auth.js'
+import morgan from 'morgan'
+import cookieParser from 'cookie-parser';
+
+const app = express()
+dotenv.config()
+app.use(morgan('tiny'))
+
+connectDB()
+app.use(cookieParser())
+app.use(express.json())
+
+app.use('/auth', auth)
+
+app.use((err, req, res,next)=>{
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "Something went wrong"
+    return res.status(errorStatus).json({
+        success:false,
+         status: errorStatus,
+         message: errorMessage,
+         stack: err.stack
+    })
+}
+)
+app.listen(8080, ()=>{
+    console.log(`port running at 8080`)
+})
