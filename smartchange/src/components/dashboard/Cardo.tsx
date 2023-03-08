02af8d21d2ebc6bc5d77ee33ad818e87bfa8dafd
Primidac <primidac@Primidac-MacBook-Pro.local>
2023-03-08 13:52:11 +0100

    Pull directly, new update in packages folder. Focus more on integrations

---
+++ smartchange/src/components/dashboard/Cardo.tsx
@@ -0,0 +1,17 @@
+import { useTheme } from '@mui/material/styles';
+import DashboardCard from '../shared/DashboardCard';
+
+
+const Cardo = () => {
+  return (
+    <DashboardCard
+      title="No Starred Merchants"
+    >
+      <>
+       
+      </>
+    </DashboardCard>
+  );
+};
+
+export default Cardo;
