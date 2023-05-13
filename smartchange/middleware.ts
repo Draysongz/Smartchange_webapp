0aa0f7b5c246ac1f91a1cdafb8a97a0c5d08ca54
Ackermann <dharmiegra4@gmail.com>
2023-05-13 19:03:22 +0100

    removed middleware

--- smartchange/middleware.ts
+++ smartchange/middleware.ts
@@ -1,40 +1,40 @@
-import {NextRequest, NextResponse} from 'next/server'
-import {verifyAuth} from './lib/auth'
+// import {NextRequest, NextResponse} from 'next/server'
+// import {verifyAuth} from './lib/auth'
 
-export async function middleware(req:NextRequest){
+// export async function middleware(req:NextRequest){
 
-    const token = req.cookies.get('myJWT')?.value
+//     const token = req.cookies.get('myJWT')?.value
 
-    const verifiedToken = 
-    token && 
-    (await verifyAuth(token).catch(err => console.log(err)))
+//     const verifiedToken = 
+//     token && 
+//     (await verifyAuth(token).catch(err => console.log(err)))
 
-    console.log('Token:', token)
-    console.log('Verified Token:', verifiedToken)
+//     console.log('Token:', token)
+//     console.log('Verified Token:', verifiedToken)
 
-    // if(req.nextUrl.pathname.startsWith('/authentication/login/') && !verifiedToken){
-    //     return
-    // }
+//     // if(req.nextUrl.pathname.startsWith('/authentication/login/') && !verifiedToken){
+//     //     return
+//     // }
 
-    const url = req.url
+//     const url = req.url
 
-    if(req.url =='/authentication/login/' && verifiedToken){
-        return NextResponse.redirect(new URL('/', url))
-    }
+//     if(req.url =='/authentication/login/' && verifiedToken){
+//         return NextResponse.redirect(new URL('/', url))
+//     }
 
-    if(req.nextUrl.pathname=='/' && !verifiedToken){
-        return NextResponse.redirect(new URL('/authentication/login/', url))
-    }
+//     if(req.nextUrl.pathname=='/' && !verifiedToken){
+//         return NextResponse.redirect(new URL('/authentication/login/', url))
+//     }
 
-    if(req.nextUrl.pathname=='/utilities/swap' && !verifiedToken){
-        return NextResponse.redirect(new URL('/authentication/login/', url))
-    }
+//     if(req.nextUrl.pathname=='/utilities/swap' && !verifiedToken){
+//         return NextResponse.redirect(new URL('/authentication/login/', url))
+//     }
 
-    if(req.nextUrl.pathname=='/utilities/chats' && !verifiedToken){
-        return NextResponse.redirect(new URL('/authentication/login/', url))
-    }
-}
+//     if(req.nextUrl.pathname=='/utilities/chats' && !verifiedToken){
+//         return NextResponse.redirect(new URL('/authentication/login/', url))
+//     }
+// }
 
-export const config={
-    matcher:['/','/authentication/login', '/utilities/swap/', '/utilities/chats/']
-}
+// export const config={
+//     matcher:['/','/authentication/login', '/utilities/swap/', '/utilities/chats/']
+// }
