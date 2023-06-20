019be103b7f15b28d91616842536c28d414248df
draysongz <dharmiegra4@gmail.com>
2023-06-20 16:00:54 +0100

    lets see

--- smartchange/pages/api/functions/chat.ts
+++ smartchange/pages/api/functions/chat.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function createChat(senderId, receiverId) {
   try {
-    const response = await axios.post('/api/chat', {
+    const response = await axios.post('https://smartchange-webapp.vercel.app//api/chat', {
       senderId: senderId,
       receiverId: receiverId
     });
